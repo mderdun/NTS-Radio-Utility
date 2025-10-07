9de42d9f3adc0f20e8f90c72608592a463a2308b
Miki Derdun <119265849+mderdun@users.noreply.github.com>
2025-10-07 20:01:07 +0100

    Initial Commit

---
+++ NTS Radio Utility/NTS_Radio_UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTS_Radio_UtilityApp.swift
+//  NTS Radio Utility
+//
+//  Created by Miki on 07/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct NTS_Radio_UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
